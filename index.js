c450930a2c352c295cf9cc4f061a287ab4b45a79
thilina15 <thibbatz@gmail.com>
2021-09-15 19:48:38 +0530

    new changes with ws

--- index.js
+++ index.js
@@ -1,51 +1,31 @@
-const webSocketsServerPort = 4000;
-const webSocketServer = require('websocket').server;
+const express = require('express');
+const app = express();
 const http = require('http');
-const express = require('express')
-const app = express()
+const WebSocket = require('ws');
 
-// Spinning the http server and the websocket server.
+const port = 6969;
 const server = http.createServer(app);
-server.listen(process.env.PORT || webSocketsServerPort) ;
-console.log('listening on port 4000');
+const wss = new WebSocket.Server({ server })
 
-
-const wsServer = new webSocketServer({
-  httpServer: server
-});
-
-const clients = {};
-
-// This code generates unique userid for everyuser.
-const getUniqueID = () => {
-  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  return s4() + s4() + '-' + s4();
-};
-
-wsServer.on('request', function (request) {
-  var userID = getUniqueID();
-  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
-
-  // You can rewrite this part of the code to accept only the requests from allowed origin
-  const connection = request.accept(null, request.origin);
-  clients[userID] = connection;
-  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
-
-  connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      console.log('Received Message: ', message.utf8Data);
-
-      // broadcasting message to all connected clients
-      for(key in clients) {
-        clients[key].sendUTF(message.utf8Data);
-        console.log('sent Message to: ', clients[key]);
+wss.on('connection', function connection(ws) {
+  console.log('client connected');
+  ws.socketID = 'user_id'
+  console.log(wss.clients.size);
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
       }
-    }
+    })
   })
-});
-
-
+})
 
 app.get('/',(req,res)=>{
-  res.send('server is running.....')
+  res.send('server is tunning....')
 })
+
+
+server.listen(process.env.PORT || port, function() {
+  console.log(`Server is listening on ${port}!`)
+})
+
