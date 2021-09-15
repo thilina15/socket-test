71471449d1e88748c0167b7e11a5251fba49f4dc
thilina15 <thibbatz@gmail.com>
2021-09-15 16:27:53 +0530

    new test without socket.io

--- index.js
+++ index.js
@@ -1,227 +1,43 @@
-if (process.env.NODE_ENV !== 'production'){
-  require('dotenv').config()   
-}
+const webSocketsServerPort = 4000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
 
-const fs = require('fs')
-const path = require('path')
-const morgan = require('morgan')
-const logger = require('socket.io-logger')
-const util = require('util')
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server,{
-cors:{
-  origin:'*',
-  methods:['GET', 'POST', 'PUT']
-}
-})
-const mongoose = require('mongoose')
-const cors = require('cors')
-const {getChatRooms} = require('./dbAccess')
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort || process.env.PORT);
+console.log('listening on port 4000');
 
-var serverHealth={}
-var clients = {}
 
-//logger details....................................................................................................
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-app.use(morgan('combined', { stream: accessLogStream }))
-
-var options = {stream: fs.createWriteStream(path.join(__dirname, 'events.log'),{flags:'a'}) };
-io.use((socket,next)=> {
-logger(options)
-next()
+const wsServer = new webSocketServer({
+  httpServer: server
 });
 
-//socket events start ----------------------------------------------------------------------------------------------
-io.on('connection', socket => {
-console.log('user connected');
-var user_ID=null
-var user_email = null
-  
-//on typing user
-socket.on('typing',(chat)=>{
-  socket.to(chat.chatroom_id).emit('typing',chat)
-})
+const clients = {};
 
-//on typingEnd user
-socket.on('typingEnd',(chat)=>{
-  socket.to(chat.chatroom_id).emit('typingEnd',chat)
-})
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
 
- //on active status
- socket.on('activeStatus',(user_id,status)=>{
-    var requestedUserOB = clients[user_id]
-    if(requestedUserOB){
-      status('online')
-    }else{
-      status('offline')
-    }
-})
+wsServer.on('request', function (request) {
+  var userID = getUniqueID();
+  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
 
-//on login user
-socket.on('login',async(userID,email)=>{
-  
-  // socket.join('test')
-  console.log('new user joining.......... ', email);
-  clients[userID] = socket
-  user_ID = userID
-  user_email = email
- 
-  //joining to rooms
-    var rooms = await getChatRooms(userID)
-    console.log('total chatromms for user: ',rooms.length);
-    rooms.forEach(element => {
-      socket.join(String(element))
-      socket.to(String(element)).emit('online',userID) //send online notification to other chat rooms
-    });
-    console.log('user login successfully.. ', email); 
-})  
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
 
- //on logout
- socket.on('logout',async()=>{ 
-  if(user_ID){
-    try{
-      var rooms = await getChatRooms(user_ID)
-      rooms.forEach(element => {
-        socket.to(String(element)).emit('offline',user_ID) //send offline notification to other chat rooms
-      });
-      delete clients[user_ID]
-    }catch(e){
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ', message.utf8Data);
 
-    }
-  }
-  console.log('user logout ', user_email);
-})   
-
-//on user disconneting
-socket.on('disconnect',async(reason)=>{
-  console.log('user disconnecting for reason ',reason);
-  if(user_ID){
-    try{
-      var rooms = await getChatRooms(user_ID)
-      rooms.forEach(element => {
-        socket.to(element).emit('offline',user_ID) //send offline notification to other chat rooms
-      });
-      delete clients[user_ID]
-    }catch(e){
-
-    }
-    console.log('user disconnected ', user_email);
-  }
-})
-
-})
-//socket events end ----------------------------------------------------------------------------------------------
-
-//databse connection
-
-mongoose.connect(process.env.DATABASE_URL,{
-useNewUrlParser: true,
-useUnifiedTopology: true,
-useFindAndModify: false, 
-useCreateIndex: true}) 
-const db = mongoose.connection 
-db.on('error', (error) =>{
-serverHealth.databaseConnection='connection fail' 
-console.log('error')
-})
-db.once('open', ()=> {
-serverHealth.databaseConnection='connection success'
-console.log('connected to mongoose')
-}) 
-
-
-
-app.use(cors())
-app.use(express.json())
-// REST end points start -------------------------------------------------------------------------------------------------------
-
-//get online connected users
-app.get('/users',(req,res)=>{
-try{
-  res.status(200).send(util.inspect(clients))
-}catch(e){
-  res.status(500).json({message:'something wrong'}) 
-}
-})
-
-//get all rooms
-app.get('/rooms',(req,res)=>{
-try{
-  let rooms = 0
-  var roomsMap = io.sockets.adapter.rooms
-  roomsMap.forEach((value,key)=>{
-    rooms++
-  })
-
-  res.status(200).json(rooms)
-}catch(e){
-  res.status(500).json({message:'something wrong'}) 
-}
-})
-
-// check live server health
-app.get('/',(req,res)=>{
-try{
-  res.status(200).json(serverHealth)
-}catch(e){
-  res.status(500).json({message:'something wrong'}) 
-}
-})
-
-// get message from message server
-app.post('/message',(req,res)=>{
-console.log('new message from message server...........',req.body);
-  try{
-    io.to(req.body.chatroom_id).emit('message',{message:req.body})
-    console.log('message sent to chatroom');
-    res.status(200).json({message:'done'})
-  }catch(e){
-    console.log('message send error');
-    res.status(400).json({message:'error'})
-  }
-})
-
-//create new chat room
-app.post('/chatroom',(req,res)=>{
-console.log('new chat room request');
-try{
-  var users = req.body.users
-  users.forEach(userID=>{
-    var socketOB = clients[userID]
-    if(socketOB){
-      socketOB.join(String(req.body.chatID))
+      // broadcasting message to all connected clients
+      for(key in clients) {
+        clients[key].sendUTF(message.utf8Data);
+        console.log('sent Message to: ', clients[key]);
+      }
     }
   })
-  res.status(200).json({message:'done'})
-  console.log('chatroom created ');
-}catch(e){
-  res.status(400).json({message:'error'})
-  console.log('chatroom created error');
-}
-
-})
-
-//download socket logs 
-app.post('/socketlog',(req,res)=>{
-const file = path.join(__dirname, 'events.log')
-res.download(file)
-})
-
-//download REST logs 
-app.post('/restlogs',(req,res)=>{
-const file = path.join(__dirname, 'access.log')
-res.download(file)
-})
-
-// REST end points end -------------------------------------------------------------------------------------------------------
-
-
-//server listen
-server.listen(process.env.PORT || 4000, function() {
-console.log('listening on port 4000')
-serverHealth.message='server is running'
-})
-
-
+});
