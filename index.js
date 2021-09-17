f9c009b4368f1be3d58419ad194944982de3670d
thilina15 <thibbatz@gmail.com>
2021-09-17 15:28:29 +0530

    6

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ app.post('/connect',(req,res)=>{
 
 app.post('/disconnect',(req,res)=>{
   delete clients[req.body.connectionID]
-  res.status(200) 
+  res.status(200).send(JSON.stringify({message:'hello world'})) 
 })
 
 app.post('/message',(req,res)=>{
   clients[req.body.connectionID] = req.body.connectionID 
   console.log(clients);
   console.log('message route', req.body);
-  res.status(200)
+  res.status(200).send(JSON.stringify({message:'reply message'})) 
 })
 
 server.listen(process.env.PORT || port, function() {
