d74c121f639b67cde80787e7045d878aea6ba514
thilina15 <thibbatz@gmail.com>
2021-09-17 16:28:28 +0530

    7

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const http = require('http');
 const WebSocket = require('ws');
 
 const port = 6969;
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server })
+// const server = http.createServer(app);
+// const wss = new WebSocket.Server({ server })
 
 var clients = {}
 
-wss.on('connection', function connection(ws) {
-  console.log('client connected');
-  ws.socketID = 'user_id'
-  console.log(wss.clients.size);
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    })
-  })
-})
+// wss.on('connection', function connection(ws) {
+//   console.log('client connected');
+//   ws.socketID = 'user_id'
+//   console.log(wss.clients.size);
+//   ws.on('message', function incoming(data) {
+//     wss.clients.forEach(function each(client) {
+//       if (client !== ws && client.readyState === WebSocket.OPEN) {
+//         client.send(data);
+//       }
+//     })
+//   })
+// })
 
 app.use(express.json())
 
@@ -47,7 +47,7 @@ app.post('/message',(req,res)=>{
   res.status(200).send(JSON.stringify({message:'reply message'})) 
 })
 
-server.listen(process.env.PORT || port, function() {
+app.listen(process.env.PORT || port, function() {
   console.log(`Server is listening on ${port}!`)
 })
 
