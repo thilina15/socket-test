ea40d3f55d75ee39594109bc0b28e0915f33d4db
thilina15 <thibbatz@gmail.com>
2021-09-21 09:46:05 +0530

    remove unused files

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const WebSocket = require('ws');
 
 const port = 6969;
-// const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server })
 
 var clients = {}
 
-// wss.on('connection', function connection(ws) {
-//   console.log('client connected');
-//   ws.socketID = 'user_id'
-//   console.log(wss.clients.size);
-//   ws.on('message', function incoming(data) {
-//     wss.clients.forEach(function each(client) {
-//       if (client !== ws && client.readyState === WebSocket.OPEN) {
-//         client.send(data);
-//       }
-//     })
-//   })
-// })
+
 
 app.use(express.json())
 
